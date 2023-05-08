a8027f3cde38d5e424dd4fc5dd58712154c67853
Jason Pinkston <jason.pinkston.it@outlook.com>
2023-05-08 17:44:45 -0500

    one last edit.

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -5,5 +5,5 @@ import { Navigate } from 'react-router-dom'
 export default function ProtectedRoute({children}) {
     const {currentUser} = useAuth()
 
-  return currentUser ? children : <Navigate to='/' />
+  return currentUser ? children : <Navigate to='/Login' />
 }
